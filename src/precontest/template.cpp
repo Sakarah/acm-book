2904c9c4f3f55ad2a3e2c623147427f6b1aef1b0
Guillaume Bertholon <guillaume.bertholon@ens.fr>
2020-01-24 16:58:37 +0100

    Adjust book for ENS cULMinante

--- src/precontest/template.cpp
+++ src/precontest/template.cpp
@@ -3,24 +3,25 @@
 #define FOR(i, n) for(lli i = 0; i < (lli)(n); ++i)
 #define FORU(i, a, b) for(lli i = (lli)(a); i < (lli)(b); ++i)
 #define FORD(i, a, b) for(lli i = (lli)(b)-1; i >= (lli)(a); --i)
+
+#define pb push_back
 #define ALL(x) (x).begin(), (x).end()
 
+#define mp make_pair
+#define fst first
+#define snd second
+
+#define mt make_tuple
 #define X(A) get<0>(A)
 #define Y(A) get<1>(A)
 #define Z(A) get<2>(A)
 #define W(A) get<3>(A)
 
-#define mt make_tuple
-#define mp make_pair
-#define pb push_back
-#define fst first
-#define snd second
-
 using namespace std;
 using lli = long long int;
 using llu = unsigned long long int;
 
-using pii   = tuple<lli, lli>;
+using pii   = pair<lli, lli>;
 using piii  = tuple<lli, lli, lli>;
 using vi    = vector<lli>;
 using vii   = vector<pii>;
